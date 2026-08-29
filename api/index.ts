3f8298cd9105c49b9df7f65a53474e23e6f88e6f
SHREYASH SINGH <shreyashsinghegi2@gmail.com>
2026-08-29 15:15:08 +0530

    Test direct Express export for Vercel

--- api/index.ts
+++ api/index.ts
@@ -1,3 +1,11 @@
-import handler from '../server/vercelHandler';
+import express from 'express';
+import { apiRouter } from '../server/routes';
+import { personalAccountRouter } from '../server/personalAccountRoutes';
 
-export default handler;
+const app = express();
+app.disable('x-powered-by');
+app.use(express.json({ limit: '2mb' }));
+app.use('/api', apiRouter);
+app.use('/api', personalAccountRouter);
+
+export default app;
